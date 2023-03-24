caac1f082c0e2121cbb81123299edaacdaec1293
OctavianIonescu <octav3000@yahoo.com>
2023-03-24 11:29:38 +0000

    Initial Project StartPoint

---
+++ stockexch/src/main/java/com/stockex/stockexch/StockexchApplication.java
@@ -0,0 +1,13 @@
+package com.stockex.stockexch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockexchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockexchApplication.class, args);
+	}
+
+}
