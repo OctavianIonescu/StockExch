8e685585d91e0dcce1aa73a8c77d637fb0d55b95
OctavianIonescu <octav3000@yahoo.com>
2023-03-27 16:24:09 +0100

    Added Service files

---
+++ stockexch/src/main/java/com/stockex/stockexch/Service/OrderServiceIMPL.java
@@ -0,0 +1,27 @@
+package com.stockex.stockexch.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.stockex.stockexch.Entities.Orders;
+@Service
+public class OrderServiceIMPL implements OrderService {
+
+    @Override
+    public Orders addOrder(Orders order) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addOrder'");
+    }
+
+    @Override
+    public int deleteOrderByID(int ID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteOrderByID'");
+    }
+
+    @Override
+    public int setFulfilledByID(int ID) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setFulfilledByID'");
+    }
+    
+}
---
+++ stockexch/src/main/java/com/stockex/stockexch/Service/Order_bookServiceIMPL.java
@@ -0,0 +1,8 @@
+package com.stockex.stockexch.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Order_bookServiceIMPL implements Order_bookService {
+    
+}
